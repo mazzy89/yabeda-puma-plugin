b0e0b07448b26c08c7bdd991d945126bdbd9f7ed
Salvatore Mazzarino <dev@mazzarino.cz>
2019-12-13 19:23:33 +0100

    Fix TCP socket
    Signed-off-by: Salvatore Mazzarino <dev@mazzarino.cz>

--- lib/yabeda/puma/plugin/statistics/fetcher.rb
+++ lib/yabeda/puma/plugin/statistics/fetcher.rb
@@ -1,5 +1,6 @@
 require 'yabeda/puma/plugin/statistics'
 require 'json'
+require 'socket'
 
 module Yabeda
   module Puma
@@ -11,18 +12,17 @@ module Yabeda
 
             if control_url.start_with? "unix://"
               path = control_url.gsub("unix://", '')
-              sock = Socket.unix(path)
+              s = UNIXSocket.new(path)
             elsif control_url.start_with? "tcp://"
               host, port = control_url.match(/^tcp:\/\/([a-z0-9\-._~%]+):([0-9]+)/).captures
-
-              sock = Socket.tcp(host, port)
+              s = TCPSocket.new(host, port)
             else
               raise ArgumentError("Unknown puma control url type #{control_url}")
             end
 
-            body = sock do |socket|
-              socket << "GET /stats?token=#{Yabeda::Puma::Plugin.control_auth_token} HTTP/1.0\r\n\r\n"
-              socket.read
+            body = s do |socket|
+              s.puts("GET /stats?token=#{Yabeda::Puma::Plugin.control_auth_token} HTTP/1.0\r\n\r") 
+              socket.gets
             end
 
             JSON.parse(body.split("\n").last)
